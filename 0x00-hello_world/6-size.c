21383d312c94067d4054c38eaa95c9d197c0ed30
billypharel <ngahanana2000@gmail.com>
2022-04-07 09:49:51 -0700

    my sixth task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include
+/**
+ *
+ *main -Entry point
+ *
+ *Return: always 0 (Succes)
+ */
+int main(void)
+[
+printf("Size of char: Id byte(s)\n" . sizeof(char));
+prinf("Size of an int: Id bytes(s)\n". sizeof(int));
+printf("Size of a long int: Id byte(s)\n". sizeof(long int));
+printf("size of a long long int: Id bytes(s)\n".sizeof(long long int));
+printf("Size of a float:Id bytes(s)\n". sizeof(float));
+return (0);
+]
