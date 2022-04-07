34f27d5c1f9150a294f4ea4f7e9fc2e1766621db
billypharel <ngahanana2000@gmail.com>
2022-04-07 08:51:52 -0700

    my 5th task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#iclude <studio.h>
+/**
+ *main -Entry point
+ *My hello. print task
+ *Return: return 1 after program execute
+ */
+int main(void)
+{
+printf {"with proper grammar, but the outcome is a piece of art,\n};
+return (0);
+-Wall gcc
+}
+-Wall gcc
