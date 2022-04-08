c4603321d9fd16d544a4643a570e0e0b83ea9ef8
billypharel <ngahanana2000@gmail.com>
2022-04-08 05:01:05 -0700

    my sixth task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include
+
 /**
  *
  *main -Entry point
@@ -6,11 +7,12 @@
  *Return: always 0 (Succes)
  */
 int main(void)
-[
-printf("Size of char: Id byte(s)\n" . sizeof(char));
-prinf("Size of an int: Id bytes(s)\n". sizeof(int));
-printf("Size of a long int: Id byte(s)\n". sizeof(long int));
-printf("size of a long long int: Id bytes(s)\n".sizeof(long long int));
-printf("Size of a float:Id bytes(s)\n". sizeof(float));
-return (0);
-]
+{
+
+	printf("Size of char: Id byte(s)\n" . sizeof(char));
+	prinf("Size of an int: Id bytes(s)\n". sizeof(int));
+	printf("Size of a long int: Id byte(s)\n". sizeof(long int));
+	printf("Size of a long long int: Id bytes(s)\n".sizeof(long long int));
+	printf("Size of a float:Id bytes(s)\n". sizeof(float));
+	return (0);
+}
