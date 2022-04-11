bd80902e432bbe491f3c5e6a37793a508eeef1cd
billypharel <ngahanana2000@gmail.com>
2022-04-11 05:06:20 -0700

    my zero task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,12 +17,12 @@ int main(void)
 
 	if (n>1)
 
-		printf("Id is positive\n", n);
+		printf("%d is positive\n", n);
 	else if (n<0)
-		printf("Id is negative\n", n);
+		printf("%d is negative\n", n);
 	else
 
-		printf("Id is zero\n", n);
+		printf("%d is zero\n", n);
 	
 	return (0);
 }
