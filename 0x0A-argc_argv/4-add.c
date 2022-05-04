71b0a9608f16503d5b8267b90cd97b6b5390cfa6
billypharel <ngahanana2000@gmail.com>
2022-05-04 05:27:21 -0700

    my fourth task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
+/**
+ * _isnumber - checks if string is a number
+ * @s: string
+ *
+ * Return: On success 1.
+ * If not a number, 0 is returned.
+ */
+
+int _isnumber(char *s)
+{
+	int i, check, d;
+
+	i = 0, d = 0, check = 1;
+	if (*s == '-')
+		i++;
+	for (; *(s + i) != 0; i++)
+	{
+		d = isdigit(*(s + i));
+		if (d == 0)
+		{
+			check = 0;
+			break;
+		}
+	}
+	return (check);
+}
 /**
  * main - Entry point
  *
@@ -10,18 +37,22 @@
  */
 int main(int argc, char **argv)
 {
-	int n, ex;
+	int i, n, ex;
 
-	ex = 0;
-	if (argc != 3)
+	ex = 0, n = 0;
+	if (argc > 1)
 	{
-		printf("%s\n", "Error");
-		ex = 1;
+		for (i = 1; i < argc; i++)
+		{
+			if (_isnumber(argv[i]))
+				n += atoi(argv[i]);
+			else
+				ex = 1;
+		}
 	}
-	else
-	{
-		n = atoi(argv[1]) * atoi(argv[2]);
+	if (ex == 0)
 		printf("%i\n", n);
-	}
+	else
+		printf("%s\n", "Error");
 	return (ex);
 }
