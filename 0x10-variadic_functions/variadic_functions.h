53fc79a88aa788e341f4da8e5f2caa45f0d7a11b
billypharel <ngahanana2000@gmail.com>
2022-05-12 09:03:59 -0700

    my first task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,22 @@
-int _putchar(char c);
+#ifndef VARFUNC_H
+#define VARFUNC_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct type - Struct data type
+ *
+ * @op: data type argument
+ * @f: The function associated
+ */
+
+typedef struct type
+{
+	char *op;
+	void (*f)(va_list, char *);
+} type_t;
+
+#endif
